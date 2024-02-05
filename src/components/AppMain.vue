7ef0b8cc71b0b9ff02568cc31e52a14285616f82
danieledisco <daniele.disco.1965@gmail.com>
2024-02-05 10:19:35 +0100

    Main Comp. not completed: trouble file svg

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,28 +2,111 @@
 export default {
     name: 'AppMain',
 }
+
 </script>
  
 <template>
     <main>
-        <div class="container">
-            <div class="black">
-                -->contents goes here <-- </div>
+        <div class="mainUp">
+            <div class="container">
+                contents goes here
             </div>
+        </div>
+        <div class="mainDown">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="cardImage">
+                            <img src="/buy-comics-digital-comics.png" alt="">
+                        </div>
+                        <div class="cardName">
+                            DIGITAL COMICS
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="cardImage">
+                            <img src="/buy-comics-merchandise.png" alt="">
+                        </div>
+                        <div class="cardName">
+                            DC MERCHANDISE
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="cardImage">
+                            <img src="/buy-comics-subscriptions.png" alt="">
+                        </div>
+                        <div class="cardName">
+                            SUBSCRIPTION
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="cardImage">
+                            <img src="/buy-dc-power-visa.svg" alt="">
+
+                        </div>
+                        <div class="cardName">
+                            DC POWER VISA
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="cardImage">
+                            <img src="/buy-comics-shop-locator.png" alt="">
+                        </div>
+                        <div class="cardName">
+                            COMIC SHOP LOCATOR
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
     </main>
 </template>
  
-<style>
+<style scoped>
 main {
-    background-color: var(--comics-dark);
     min-height: 600px;
+}
 
-    & .black {
-        height: 300px;
+.mainUp {
+    background-color: var(--comics-dark);
+    height: 300px;
+    color: var(--comics-light);
+    font-size: 2rem;
+    font-weight: 700;
+    padding: 8rem 0;
+}
+
+.mainDown {
+    background-color: var(--comics-blue);
+    padding: 4rem 0;
+
+    & .col {
+        display: flex;
         color: var(--comics-light);
-        font-size: 2rem;
-        font-weight: 700;
-        padding: 8rem 0;
+
+        & .cardImage {
+            text-align: center;
+            width: var(--comics-card-image-width);
+            color: var(--comics-light);
+
+        }
+
+        & .cardName {
+            font-size: 0.8rem;
+            text-align: center;
+            margin-top: auto;
+            margin-bottom: auto;
+            margin-left: 0.5rem;
+            white-space: nowrap;
+        }
+
     }
+}
+
+.svg {
+    z-index: 100;
 }
 </style>
